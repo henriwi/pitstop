7c601d9c3005a0374a3296ca861d58f7037e54fb
henrikwingerei <henrikwingerei@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-01-20 14:34:17 +0000

    lagt til getTiers i HelloController og testet denne

--- miniapp/src/main/java/no/example/controller/HelloController.java
+++ miniapp/src/main/java/no/example/controller/HelloController.java
@@ -1,6 +1,12 @@
 package no.example.controller;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import no.example.domain.Tire;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +16,16 @@ public class HelloController {
 	public String doPage()
 	{
 		return "hello";
+	}
+	
+	@ModelAttribute("tiers")
+	public List<Tire> getTiers()
+	{
+		List<Tire> list = new LinkedList<Tire>();
+		Tire t = new Tire();
+		t.setId(1);
+		t.setName("pirelli");
+		list.add(t);
+		return list;
 	}
 }
