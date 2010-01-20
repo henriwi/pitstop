98e3e0af6999d13c0ebc39eee79c4e3b6b56e557
a.evenstuen <a.evenstuen@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-01-20 13:39:07 +0000

    La til controller og test

--- trunk/miniapp/src/main/java/no/example/controller/HelloController.java
+++ trunk/miniapp/src/main/java/no/example/controller/HelloController.java
@@ -1,8 +1,14 @@
 package no.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HelloController {
+	
+	@RequestMapping("/hello")
 	public String doPage()
 	{
-		return "Hello";
+		return "hello";
 	}
 }
