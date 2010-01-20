95bba25521fa6f9d311e5698712d6f6ac766c1ca
eirik.luka <eirik.luka@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-01-20 13:18:42 +0000

    Created controller and controller-test.

---
+++ miniapp/src/test/java/HelloControllerTest.java
@@ -0,0 +1,14 @@
+
+import static org.junit.Assert.*;
+import no.example.controller.HelloController;
+
+import org.junit.Test;
+
+public class HelloControllerTest {
+	@Test
+	public void doPageTest()
+	{
+		HelloController hc = new HelloController();
+		assertEquals("Hello", hc.doPage());
+	}
+}
