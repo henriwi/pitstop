4006450a71073e44addd69497b530efe89032150
jt.dalen <jt.dalen@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-01-28 10:15:22 +0000

    laget test til Tire

--- pitstop/test/unit/no/pstop/webapp/TireTests.groovy
+++ pitstop/test/unit/no/pstop/webapp/TireTests.groovy
@@ -3,15 +3,27 @@ package no.pstop.webapp
 import grails.test.*
 
 class TireTests extends GrailsUnitTestCase {
-    protected void setUp() {
-        super.setUp()
-    }
-
-    protected void tearDown() {
-        super.tearDown()
-    }
-
-    void testSomething() {
-
-    }
+	protected void setUp() {
+		super.setUp()
+	}
+	
+	protected void tearDown() {
+		super.tearDown()
+	}
+	
+	void generateTire() {
+		Tire t = new Tire(width:192,profile:60,construction:"R",diameter:17,partNr:"123AB",
+		loadIndex:165,speedIndex:"H",pattern:"m12",tireType:"sommer")
+		assertEquals 192, t.width
+		assertEquals 60, t.profile
+		assertEquals "R", t.construction
+		assertEquals 17, t.diameter
+		assertEquals "123AB", t.partNr
+		assertEquals 165, t.loadIndex
+		assertEquals "H", t.speedIndex
+		assertEquals "m12", t.pattern
+		assertEquals "sommer", t.tireType
+		
+		
+	}
 }
