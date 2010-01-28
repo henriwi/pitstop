6217501aaaf464bb32c6ce81e8a0bd18057a56ba
jt.dalen <jt.dalen@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-01-28 11:59:06 +0000

    Endret int til Integer i Tire modellen

--- pitstop/grails-app/domain/no/pstop/webapp/Tire.groovy
+++ pitstop/grails-app/domain/no/pstop/webapp/Tire.groovy
@@ -3,11 +3,11 @@ package no.pstop.webapp
 class Tire {
 	long id
 	String partNr
-	int width
-	int profile
-	char construction
-	int diameter
-	int loadIndex
+	Integer width
+	Integer profile
+	Character construction
+	Integer diameter
+	Integer loadIndex
 	String speedIndex
 	String pattern
 	String tireType
