b71fa45e3f4de5898433a3d589a07b15fb07f57f
eirik.luka <eirik.luka@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-02-02 14:18:50 +0000

    Oppdatert noen feilmeldinger

--- trunk/pitstop/grails-app/controllers/no/pstop/webapp/TireController.groovy
+++ trunk/pitstop/grails-app/controllers/no/pstop/webapp/TireController.groovy
@@ -84,16 +84,16 @@ class TireController {
         if (tireInstance) {
             try {
                 tireInstance.delete(flush: true)
-                flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'tire.label', default: 'Dekk'), params.partNr])}"
+                flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'tire.label', default: 'Dekk'), params.id])}"
                 redirect(action: "list")
             }
             catch (org.springframework.dao.DataIntegrityViolationException e) {
-                flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'tire.label', default: 'Dekk'), params.partNr])}"
+                flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'tire.label', default: 'Dekk'), params.id])}"
                 redirect(action: "show", id: params.id)
             }
         }
         else {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'tire.label', default: 'Dekk'), params.partNr])}"
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'tire.label', default: 'Dekk'), params.id])}"
             redirect(action: "list")
         }
     }
