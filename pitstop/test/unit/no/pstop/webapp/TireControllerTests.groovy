b15d42538403631e9beabd1905e2e106d931882b
henrikwingerei <henrikwingerei@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-02-02 09:01:43 +0000

    First commit

---
+++ pitstop/test/unit/no/pstop/webapp/TireControllerTests.groovy
@@ -0,0 +1,17 @@
+package no.pstop.webapp
+
+import grails.test.*
+
+class TireControllerTests extends ControllerUnitTestCase {
+	protected void setUp() {
+		super.setUp()
+	}
+	
+	protected void tearDown() {
+		super.tearDown()
+	}
+	
+	void testSomething() {
+		
+	}
+}
