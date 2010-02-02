9bc0162880d139a00c775a0b8329eac50d5f5fd0
henrikwingerei <henrikwingerei@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-02-02 09:01:43 +0000

    First commit

---
+++ trunk/pitstop/grails-app/domain/no/pstop/webapp/Tire.groovy
@@ -0,0 +1,18 @@
+package no.pstop.webapp
+
+class Tire {
+	long id
+	String partNr
+	Integer width
+	Integer profile
+	Character construction
+	Integer diameter
+	Integer loadIndex
+	String speedIndex
+	String pattern
+	String tireType
+	
+	static constraints = {
+		width(range:100..350)
+	}
+}
