190c78f3f0cb3faf8eea1cd99fcb8777a21c30c4
oyvind.midtbo <oyvind.midtbo@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-02-03 12:52:17 +0000

    Lagt til domeneobjektet TireOccurrence

---
+++ trunk/pitstop/grails-app/domain/no/pstop/webapp/TireOccurrence.groovy
@@ -0,0 +1,16 @@
+package no.pstop.webapp
+
+class TireOccurrence {
+	//long id
+	Tire tire
+	int numberInStock
+	int price
+	int numberOfReserved
+	int numberOfOrdered
+	//TODO
+	//DateTime dato
+	int numberOfAvailable = numberInStock - numberOfReserved
+	
+    static constraints = {
+    }
+}
