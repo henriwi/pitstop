3c3d56c051ac84f2a82310a64feb140ac5b289c6
oyvind.midtbo <oyvind.midtbo@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-02-03 12:52:17 +0000

    Lagt til domeneobjektet TireOccurrence

---
+++ pitstop/test/unit/no/pstop/webapp/TireOccurenceTests.groovy
@@ -0,0 +1,17 @@
+package no.pstop.webapp
+
+import grails.test.*
+
+class TireOccurenceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
