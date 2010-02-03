1c7a7f70b689d5ef96c7622579e59bfbd102c6f4
oyvind.midtbo <oyvind.midtbo@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-02-03 13:16:10 +0000

    Ferdig

--- trunk/pitstop/grails-app/domain/no/pstop/webapp/TireOccurrence.groovy
+++ trunk/pitstop/grails-app/domain/no/pstop/webapp/TireOccurrence.groovy
@@ -1,15 +1,12 @@
 package no.pstop.webapp
 
 class TireOccurrence {
-	//long id
 	Tire tire
 	Integer numberInStock
 	Integer price
 	Integer numberOfReserved
 	Integer numberOfOrdered
-	//Date fungerer ikke
 	Date date
-	//Ikke sikkert vi trenger numberOfAvailable
 	Integer numberOfAvailable
 	
     static constraints = {
