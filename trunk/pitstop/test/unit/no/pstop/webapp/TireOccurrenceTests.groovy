77a3916f80028f3747f7ae518e4968ed8ea58e02
oyvind.midtbo <oyvind.midtbo@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-02-03 13:44:05 +0000

    h ho captain Hey !

--- trunk/pitstop/test/unit/no/pstop/webapp/TireOccurrenceTests.groovy
+++ trunk/pitstop/test/unit/no/pstop/webapp/TireOccurrenceTests.groovy
@@ -10,8 +10,23 @@ class TireOccurrenceTests extends GrailsUnitTestCase {
     protected void tearDown() {
         super.tearDown()
     }
-
-    void testSomething() {
-
-    }
+	
+	void testSomething() {
+		
+	}
+	
+	void testGenerateTireOccurrence() {
+		/*def tire = new Tire(width:192,profile:60,construction:"R",diameter:17,partNr:"123AB",
+		 loadIndex:165,speedIndex:"H",pattern:"m12",tireType:"Sommer")*/
+		def tireOccurrence = new TireOccurrence(tire:new Tire(),price:89.89,numberInStock:4,numberOfReserved:3,
+		numberOfOrdered:8,date:new Date(), numberOfAvailable:1 )
+		
+		
+		assertEquals 89.89, tireOccurrence.price
+		assertEquals 4, tireOccurrence.numberInStock
+		assertEquals 3, tireOccurrence.numberOfReserved
+		assertEquals 8, tireOccurrence.numberOfOrdered
+		assertEquals new Date(), tireOccurrence.date
+		assertEquals 1, tireOccurrence.numberOfAvailable
+	}
 }
