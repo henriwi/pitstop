dc10221807fb1d4e01fb9bb7d368a90741c5e7ef
henrikwingerei <henrikwingerei@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-02-04 14:24:13 +0000

    Fikset testen i TireOccurence, dato feltet

--- pitstop/test/unit/no/pstop/webapp/TireOccurrenceTests.groovy
+++ pitstop/test/unit/no/pstop/webapp/TireOccurrenceTests.groovy
@@ -13,23 +13,15 @@ class TireOccurrenceTests extends GrailsUnitTestCase {
     }
 	
 	void testGenerateTireOccurrence() {
-		/*def tire = new Tire(width:192,profile:60,construction:"R",diameter:17,partNr:"123AB",
-		 loadIndex:165,speedIndex:"H",pattern:"m12",tireType:"Sommer")*/
-		
-		def date = new SimpleDateFormat("MM/dd/yyyy")
-		
-		//def template = '<g ateFormat format="dd-MM-yyyy" date="${myDate}" />'
-		//def d = new Date()
-
+		def df = new SimpleDateFormat("MM/dd/yyyy")
 		def tireOccurrence = new TireOccurrence(tire:new Tire(),price:89.89,numberInStock:4,numberOfReserved:3,
-				numberOfOrdered:8,date: date.parse("10/1/2008"), numberOfAvailable:1 )
+				numberOfOrdered:8,date: df.parse("10/01/2008"), numberOfAvailable:1 )
 		
 		assertEquals 89.89, tireOccurrence.price
 		assertEquals 4, tireOccurrence.numberInStock
 		assertEquals 3, tireOccurrence.numberOfReserved
 		assertEquals 8, tireOccurrence.numberOfOrdered
-		//assertOutputEquals( '01-01-2008', template, [myDate ] )
-		//assertEquals "10/1/2008", tireOccurrence.date
+		assertEquals "10/01/2008", tireOccurrence.date.format("MM/dd/yyyy")
 		assertEquals 1, tireOccurrence.numberOfAvailable
 	}
 }
