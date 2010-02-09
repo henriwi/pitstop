0842a51f32acb5bfc7a2d6047d58fe585cbb3335
henrikwingerei <henrikwingerei@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-02-09 13:49:52 +0000

    Lagt til TireOccurenceControllerTests

---
+++ pitstop/test/unit/no/pstop/webapp/TireOccurenceControllerTests.groovy
@@ -0,0 +1,15 @@
+package no.pstop.webapp;
+
+import grails.test.ControllerUnitTestCase;
+
+class TireOccurenceControllerTests extends ControllerUnitTestCase {
+
+	protected void setUp() {
+		super.setUp();
+	}
+
+	protected void tearDown() {
+		super.tearDown();
+	}
+
+}
