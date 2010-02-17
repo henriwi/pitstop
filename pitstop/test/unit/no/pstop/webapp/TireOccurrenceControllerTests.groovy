8f75dc6c5a4d1224650b5fe94506ca4bedcb6a6d
henrikwingerei <henrikwingerei@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-02-17 14:06:14 +0000

    Refaktorert TireOccurrenceControllerTests

--- pitstop/test/unit/no/pstop/webapp/TireOccurrenceControllerTests.groovy
+++ pitstop/test/unit/no/pstop/webapp/TireOccurrenceControllerTests.groovy
@@ -1,15 +1,28 @@
 package no.pstop.webapp;
 
-import grails.test.ControllerUnitTestCase;
+import grails.test.*;
+import java.util.Date;
 
 class TireOccurrenceControllerTests extends ControllerUnitTestCase {
-
+	def tire, tireOccurrence
+	
 	protected void setUp() {
 		super.setUp();
+		tire = new Tire(id:1,width:192,profile:60,construction:"R",diameter:17,partNr:"123AB",
+				loadIndex:165,speedIndex:"H",pattern:"m12",tireType:"Sommer",brand:"Pirelli",tireName:"T-Zero")
+		mockDomain TireOccurrence
+		tireOccurrence = new TireOccurrence(id:1,tire:tire,price:60.5,numberInStock:1,registrationDate:new Date())
 	}
-
+	
 	protected void tearDown() {
 		super.tearDown();
+	}
+	
+	private setParams(Tire tire, Double price, Integer numberInStock, Date registrationDate) {
+		controller.params.tire = tire
+		controller.params.price = price
+		controller.params.numberInStock = numberInStock
+		controller.params.registrationDate = registrationDate
 	}
 	
 	void testIndex() {
@@ -18,48 +31,29 @@ class TireOccurrenceControllerTests extends ControllerUnitTestCase {
 	}
 	
 	void testCreate() {
-		mockDomain TireOccurrence
 		def model = controller.create()
 		assertNotNull model.tireOccurrenceInstance
 	}
 	
 	void testSaveWithValidTireOccurrence() {
-		mockDomain TireOccurrence
-		def tire = new Tire()
-		
-		controller.params.tire = tire
-		controller.params.price = 60.5
-		controller.params.numberInStock = 0
-		controller.params.numberOfReserved = 0
-		controller.params.numberOfOrdered = 0
-		controller.params.registrationDate = new Date()
-		controller.params.numberOfAvailable = 0
+		setParams tire, 60.5, 4, new Date()
 		controller.metaClass.message = {args -> println "message: ${args}"} 
 		controller.save()
 		
-		assertEquals('redirect action', "show", redirectArgs.action)
-		assertEquals('redirect id', 1, controller.redirectArgs.id)
+		assertEquals "redirect action", "show", controller.redirectArgs.action
+		assertEquals "redirect id", 1, controller.redirectArgs.id
 	}
 	
 	void testSaveWithInvalidTireOccurrence() {
-		mockDomain TireOccurrence
-		def tire = new Tire()
-		
-		controller.params.tire = tire
-		//Unvalid price
-		controller.params.price = -10
-		controller.params.numberInStock = 0
-		controller.params.registrationDate = new Date()
+		setParams tire, -10, 0, new Date()
 		controller.metaClass.message = {args -> println "message: ${args}"} 
 		controller.save()
 		
-		assertEquals('redirect action', "create", controller.modelAndView.viewName)
-		assertNotNull('TireOccurrence should not be null', controller.modelAndView.model.linkedHashMap.tireOccurrenceInstance)
+		assertEquals "redirect action", "create", controller.modelAndView.viewName
+		assertNotNull "TireOccurrence should not be null", controller.modelAndView.model.linkedHashMap.tireOccurrenceInstance
 	}
 	
 	void testShowWithValidId() {
-		def tire = new Tire()
-		def tireOccurrence = new TireOccurrence(id:1,tire:tire,price:60.5,numberInStock:1,registrationDate:new Date())
 		mockDomain TireOccurrence, [tireOccurrence]
 		controller.params.id = 1
 		def model = controller.show()
@@ -67,91 +61,71 @@ class TireOccurrenceControllerTests extends ControllerUnitTestCase {
 	}
 	
 	void testShowWithInvalidId() {
-		def tire = new Tire()
-		def tireOccurrence = new TireOccurrence(id:1,tire:tire,price:60.5,numberInStock:1,registrationDate:new Date())
 		mockDomain TireOccurrence, [tireOccurrence]
-		controller.params.id = 2
+		controller.params.id = 999
 		controller.metaClass.message = {args -> println "message: ${args}"} 
 		controller.show()
 		assertEquals "list", controller.redirectArgs.action
 	}
 	
 	void testEditWithValidId() {
-		def tire = new Tire()
-		def expectedTireOccurrence = new TireOccurrence(id:1,tire:tire,price:60.5,numberInStock:1,registrationDate:new Date())
-		mockDomain(TireOccurrence, [expectedTireOccurrence])
+		mockDomain(TireOccurrence, [tireOccurrence])
 		controller.params.id = 1
 		Map model = controller.edit()
-		assertEquals('expected Tire', expectedTireOccurrence, model.tireOccurrenceInstance)
+		assertEquals "expected Tire", tireOccurrence, model.tireOccurrenceInstance
 	}
 	
 	void testEditWithInvalidId() {
-		mockDomain(TireOccurrence)
-		controller.params.id = 999 // no Tire with id 999 exists
+		controller.params.id = 999
 		controller.metaClass.message = {args -> println "message: ${args}"} 
 		controller.edit()
-		assertEquals('redirect action', "list", redirectArgs.action)
+		assertEquals "redirect action", "list", controller.redirectArgs.action
 	}
 	
 	void testUpdateWithValidId() {
-		def tire = new Tire()
-		def expectedTireOccurrence = new TireOccurrence(id:1,tire:tire,price:60.5,numberInStock:1,registrationDate:new Date())
-		mockDomain(TireOccurrence, [expectedTireOccurrence])
-		
-		// mock out save and hasErrors behavior for happy path
-		expectedTireOccurrence.metaClass.save = {-> return true }
-		expectedTireOccurrence.metaClass.hasErrors = {-> return false }
+		mockDomain(TireOccurrence, [tireOccurrence])
 		
 		controller.params.id = 1
 		controller.metaClass.message = {args -> println "message: ${args}"} 
 		controller.update()
 		
-		assertEquals('redirect action', "show", redirectArgs.action)
-		assertEquals('redirect id', 1, redirectArgs.id)
+		assertEquals "redirect action", "show", controller.redirectArgs.action
+		assertEquals "redirect id", 1, controller.redirectArgs.id
 	}
 	
 	void testUpdateWithValidIdButErrors() {
-		def tire = new Tire()
-		def expectedTireOccurrence = new TireOccurrence(id:1,tire:tire,price:60.5,numberInStock:1,registrationDate:new Date())
-		mockDomain(TireOccurrence, [expectedTireOccurrence])
+		mockDomain(TireOccurrence, [tireOccurrence])
 		
-		// mock out save and hasErrors behavior for happy path
-		expectedTireOccurrence.metaClass.save = {-> return false }
-		expectedTireOccurrence.metaClass.hasErrors = {-> return true }
+		tireOccurrence.metaClass.hasErrors = {-> return true }
 		
 		controller.params.id = 1
 		controller.metaClass.message = {args -> println "message: ${args}"} 
 		controller.update()
 		
-		assertEquals('redirect action', "edit", controller.modelAndView.viewName)
-		assertNotNull('TireOccurence should not be null', controller.modelAndView.model.linkedHashMap.tireOccurrenceInstance)
+		assertEquals "redirect action", "edit", controller.modelAndView.viewName
+		assertNotNull "TireOccurence should not be null", controller.modelAndView.model.linkedHashMap.tireOccurrenceInstance
 	}
 	
 	void testUpdateWithInvalidId() {
-		mockDomain(TireOccurrence)
-		
 		controller.params.id = 999
 		controller.metaClass.message = {args -> println "message: ${args}"} 
 		controller.update()
 		
-		assertEquals('redirect action', "list", redirectArgs.action)
+		assertEquals "redirect action", "list", controller.redirectArgs.action
 	}
 	
 	void testDeleteWithValidId() {
-		def tire = new Tire()
-		def tireOccurrence = new TireOccurrence(id:1,tire:tire,price:60.5,numberInStock:1,registrationDate:new Date())
 		mockDomain(TireOccurrence, [tireOccurrence])
 		controller.params.id = 1
 		controller.metaClass.message = {args -> println "message: ${args}"} 
 		controller.delete()
-		assertEquals('redirect action', "list", redirectArgs.action)
+		assertEquals "redirect action", "list", controller.redirectArgs.action
 	}
 	
 	void testDeleteWithInvalidId() {
-		mockDomain(TireOccurrence)
-		controller.params.id = 999 // no object with id 999 exists
+		controller.params.id = 999
 		controller.metaClass.message = {args -> println "message: ${args}"} 
 		controller.delete()
-		assertEquals('redirect action', "list", redirectArgs.action)
+		assertEquals "redirect action", "list", controller.redirectArgs.action
 	}
 }
