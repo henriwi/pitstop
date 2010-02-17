f290a3bcdd7bf51d9b4391552892373e44de033f
eirik.luka <eirik.luka@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-02-17 09:02:30 +0000

    Lagt til Driver og oppdatert Datasource

--- trunk/pitstop/grails-app/conf/DataSource.groovy
+++ trunk/pitstop/grails-app/conf/DataSource.groovy
@@ -13,7 +13,7 @@ hibernate {
 environments {
 	development {
 		dataSource {
-			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+			dbCreate = "create" // one of 'create', 'create-drop','update'
 			url = "jdbc:mysql://localhost/pitstop"
 		}
 	}
