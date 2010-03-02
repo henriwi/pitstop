0db2908e7d9d791062e2718ccc14558406743f2e
eirik.luka <eirik.luka@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-03-02 15:32:23 +0000

    Har laget noen tester for TireHotelOccurence

---
+++ pitstop/grails-app/domain/no/pstop/webapp/TireHotelOccurence.groovy
@@ -0,0 +1,17 @@
+package no.pstop.webapp
+
+class TireHotelOccurence {
+	
+	String tireLocation
+	String registrationNumber
+	String carType
+	Customer customer
+	String tireType
+	Date inDate
+	Date outDate
+	String notice
+	
+    static constraints = {
+		tireLocation(blank: false, matches:"\\w[\\-\\_\\w]+")
+    }
+}
