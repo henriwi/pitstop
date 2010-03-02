ac43bcc70357bc156a311f4116296901cf2b4806
eirik.luka <eirik.luka@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-03-02 12:35:24 +0000

    Har laget domenemodell og tester for Customer

---
+++ pitstop/grails-app/domain/no/pstop/webapp/Customer.groovy
@@ -0,0 +1,26 @@
+package no.pstop.webapp
+
+class Customer {
+	
+	String firstName
+	String lastName
+	String phoneNumber
+	String address
+	String postalCode
+	String city
+	String email
+	String company
+	String notice
+	
+    static constraints = {
+		firstName(blank:false, matches: "[a-zA-Z0-9æøåÆØÅ\\s\\-\\_]{0,40}")
+		lastName(blank:false, matches: "[a-zA-Z0-9æøåÆØÅ\\s\\-\\_]{0,40}")
+		phoneNumber(matches: "[\\+]?\\d{0,15}")
+		address(matches: "[a-zA-Z0-9æøåÆØÅ\\s\\-\\.\\_]{0,50}")
+		postalCode(matches: "\\d{0,10}")
+		city(matches:"[a-zA-Z0-9æøåÆØÅ\\s\\-\\_]{0,40}")
+		email(email:true)
+		company(matches: "[a-zA-Z0-9æøåÆØÅ\\s\\-\\.\\_]{0,50}")
+		notice(size:0..1000)
+    }
+}
