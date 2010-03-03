c831abbc5041c5642108da39d199c52282b1de1c
eirik.luka <eirik.luka@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-03-03 09:20:32 +0000

    Har laget tester og constraints for TireHotelOccurence

--- trunk/pitstop/grails-app/domain/no/pstop/webapp/TireHotelOccurence.groovy
+++ trunk/pitstop/grails-app/domain/no/pstop/webapp/TireHotelOccurence.groovy
@@ -12,6 +12,11 @@ class TireHotelOccurence {
 	String notice
 	
     static constraints = {
-		tireLocation(blank: false, matches:"\\w[\\-\\_\\w]+")
+		tireLocation(blank: false, nullable: false, matches:"(a-zA-ZøæåØÆÅ0-9)[a-zA-ZøæåØÆÅ0-9\\-\\_]{0,20}")
+		registrationNumber(blank: false, nullable: false, matches:"[a-zA-ZøæåØÆÅ0-9]+[\\-\\s]?[a-zA-ZøæåØÆÅ0-9]{0,7}")
+		carType(matches:"[a-zA-ZøæåØÆÅ0-9\\-\\_\\s]{0,30}")
+		tireType(blank: false, nullable: false, matches:"[a-zA-ZøæåØÆÅ0-9\\-\\(\\)\\/\\+\\s]{0,30}")
+		inDate(blank:false, nullable: false)
+		notice(size:0..1000)
     }
 }
--- trunk/pitstop/test/unit/no/pstop/webapp/TireHotelOccurenceTests.groovy
+++ trunk/pitstop/test/unit/no/pstop/webapp/TireHotelOccurenceTests.groovy
@@ -48,4 +48,64 @@ class TireHotelOccurenceTests extends GrailsUnitTestCase {
 		def badField = tireHotelOccurence.errors.getFieldError('tireLocation')
 		assertNotNull "I'm expecting to find error in tireLocation", badField
 	}
+	
+	void testGenerateTireHotelOccurrenceWithInvalidRegistationNumber() {
+		tireHotelOccurence.registrationNumber = "?"
+		
+		assertFalse "Test failed, expecting invalid registrationNumber", tireHotelOccurence.validate()
+		def badField = tireHotelOccurence.errors.getFieldError('registrationNumber')
+		assertNotNull "I'm expecting to find error in registrationNumber", badField
+	}
+	
+	void testGenerateTireHotelOccurrenceWithInvalidCarType() {
+		tireHotelOccurence.carType = "?"
+		
+		assertFalse "Test failed, expecting invalid carType", tireHotelOccurence.validate()
+		def badField = tireHotelOccurence.errors.getFieldError('carType')
+		assertNotNull "I'm expecting to find error in carType", badField
+	}
+	
+	void testGenerateTireHotelOccurrenceWithInvalidCustomer() {
+		tireHotelOccurence.customer = null
+		
+		assertFalse "Test failed, expecting invalid customer", tireHotelOccurence.validate()
+	}
+	
+	void testGenerateTireHotelOccurrenceWithInvalidTireType() {
+		tireHotelOccurence.tireType = "?"
+		
+		assertFalse "Test failed, expecting invalid tireType", tireHotelOccurence.validate()
+		def badField = tireHotelOccurence.errors.getFieldError('tireType')
+		assertNotNull "I'm expecting to find error in tireType", badField
+	}
+	
+	/*
+	void testGenerateTireHotelOccurrenceWithInvalidInDate() {
+		tireHotelOccurence.inDate = "" 
+		
+		assertFalse "Test failed, expecting invalid inDate", tireHotelOccurence.validate()
+		def badField = tireHotelOccurence.errors.getFieldError('inDate')
+		assertNotNull "I'm expecting to find error in inDate", badField
+	}
+	
+	void testGenerateTireHotelOccurrenceWithInvalidOutDate() {
+		tireHotelOccurence.outDate = null
+		
+		assertFalse "Test failed, expecting invalid outDate", tireHotelOccurence.validate()
+		def badField = tireHotelOccurence.errors.getFieldError('outDate')
+		assertNotNull "I'm expecting to find error in outDate", badField
+	}
+	*/
+	
+	void testGenerateCustomerWithInvalidNotice() {
+		
+		for(int i = 0; i < 2000; i++)
+		{
+			tireHotelOccurence.notice += "A"
+		}
+		
+		assertFalse "Test failed, expecting invalid notice", tireHotelOccurence.validate()
+		def badField = customer.errors.getFieldError("notice")
+		assertNotNull "I'm expecting to find error in notice", badField
+	}
 }
