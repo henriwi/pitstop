00c86d4959c07bdab33ea6765a67bfff13036485
eirik.luka <eirik.luka@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-03-03 14:16:09 +0000

    Oppdatert regex

--- trunk/pitstop/grails-app/domain/no/pstop/webapp/TireHotelOccurrence.groovy
+++ trunk/pitstop/grails-app/domain/no/pstop/webapp/TireHotelOccurrence.groovy
@@ -12,7 +12,7 @@ class TireHotelOccurrence {
 	String notice
 	
     static constraints = {
-		tireLocation(blank: false, nullable: false, matches:"([]a-zA-ZøæåØÆÅ0-9])[a-zA-ZøæåØÆÅ0-9\\-\\_]{0,20}")
+		tireLocation(blank: false, nullable: false, matches:"([a-zA-ZøæåØÆÅ0-9])[a-zA-ZøæåØÆÅ0-9\\-\\_]{0,20}")
 		registrationNumber(blank: false, nullable: false, matches:"[a-zA-ZøæåØÆÅ0-9]+[\\-\\s]?[a-zA-ZøæåØÆÅ0-9]{0,7}")
 		carType(matches:"[a-zA-ZøæåØÆÅ0-9\\-\\_\\s]{0,30}")
 		tireType(blank: false, nullable: false, matches:"[a-zA-ZøæåØÆÅ0-9\\-\\(\\)\\/\\+\\s]{0,30}")
