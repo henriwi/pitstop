d67b4c8a31ab7f87a34f766aff69d22e46a3f0ee
a.evenstuen <a.evenstuen@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-03-11 09:54:10 +0000

    Oppdatert list i customercontroller for søk og testet den.

--- pitstop/grails-app/controllers/no/pstop/webapp/CustomerController.groovy
+++ pitstop/grails-app/controllers/no/pstop/webapp/CustomerController.groovy
@@ -3,93 +3,109 @@ package no.pstop.webapp
 import grails.converters.JSON
 
 class CustomerController {
-
-    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
-
-    def index = {
-        redirect(action: "list", params: params)
-    }
-
-    def list = {
-        params.max = Math.min(params.max ? params.int('max') : 10, 100)
-        [customerInstanceList: Customer.list(params), customerInstanceTotal: Customer.count()]
-    }
-
-    def create = {
-        def customerInstance = new Customer()
-        customerInstance.properties = params
-        return [customerInstance: customerInstance]
-    }
-
-    def save = {
-        def customerInstance = new Customer(params)
-        if (customerInstance.save(flush: true)) {
-            flash.message = "${message(code: 'customer.created.message', args: [message(code: 'customer.label' ), customerInstance.firstName, customerInstance.lastName])}"
-            redirect(action: "show", id: customerInstance.id)
-        }
-        else {
-            render(view: "create", model: [customerInstance: customerInstance])
-        }
-    }
-
-    def show = {
-        def customerInstance = Customer.get(params.id)
-        if (!customerInstance) {
-            flash.message = "${message(code: 'customer.not.found.message', args: [message(code: 'customer.label', default: 'Customer'), params.id])}"
-            redirect(action: "list")
-        }
-        else {
-            [customerInstance: customerInstance]
-        }
-    }
-
-    def edit = {
-        def customerInstance = Customer.get(params.id)
-        if (!customerInstance) {
-            flash.message = "${message(code: 'customer.not.found.message', args: [message(code: 'customer.label', default: 'Customer'), params.id])}"
-            redirect(action: "list")
-        }
-        else {
-            return [customerInstance: customerInstance]
-        }
-    }
-
-    def update = {
-        def customerInstance = Customer.get(params.id)
-        if (customerInstance) {
-            customerInstance.properties = params
-            if (!customerInstance.hasErrors() && customerInstance.save(flush: true)) {
-                flash.message = "${message(code: 'customer.updated.message', args: [message(code: 'customer.label'), customerInstance.firstName, customerInstance.lastName])}"
-                redirect(action: "show", id: customerInstance.id)
-            }
-            else {
-                render(view: "edit", model: [customerInstance: customerInstance])
-            }
-        }
-        else {
-            flash.message = "${message(code: 'customer.not.found.message', args: [message(code: 'customer.label', default: 'Customer'), params.id])}"
-            redirect(action: "list")
-        }
-    }
-
-    def delete = {
-        def customerInstance = Customer.get(params.id)
-        if (customerInstance) {
-            try {
-                customerInstance.delete(flush: true)
-                flash.message = "${message(code: 'customer.deleted.message', args: [message(code: 'customer.label'), customerInstance.firstName, customerInstance.lastName])}"
-                redirect(action: "list")
-            }
-            catch (org.springframework.dao.DataIntegrityViolationException e) {
-                flash.message = "${message(code: 'customer.not.deleted.message', args: [message(code: 'customer.label', default: 'Customer'), params.id])}"
-                redirect(action: "show", id: params.id)
-            }
-        }
-        else {
-            flash.message = "${message(code: 'customer.not.found.message', args: [message(code: 'customer.label', default: 'Customer'), params.id])}"
-            redirect(action: "list")
-        }
-    }
+	
+	static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+	
+	def index = {
+		redirect(action: "list", params: params)
+	}
+	
+	def list = {
+		def customerList
+		def customerCount
+		
+		if(params.q){
+			customerList = Customer.search("*" + params.q + "*", escape: true).results
+			customerCount = customerList.size()
+		}
+		else {
+			params.max = Math.min(params.max ? params.int('max') : 10, 100)
+			customerList = Customer.list(params)
+			customerCount = Customer.count()
+		}
+		
+		if(customerCount == 0 ) {
+			flash.message = "Fant ingen kunder."
+		}
+		
+		[customerInstanceList: customerList, customerInstanceTotal: customerCount]
+	}
+	
+	def create = {
+		def customerInstance = new Customer()
+		customerInstance.properties = params
+		return [customerInstance: customerInstance]
+	}
+	
+	def save = {
+		def customerInstance = new Customer(params)
+		if (customerInstance.save(flush: true)) {
+			flash.message = "${message(code: 'customer.created.message', args: [message(code: 'customer.label' ), customerInstance.firstName, customerInstance.lastName])}"
+			redirect(action: "show", id: customerInstance.id)
+		}
+		else {
+			render(view: "create", model: [customerInstance: customerInstance])
+		}
+	}
+	
+	def show = {
+		def customerInstance = Customer.get(params.id)
+		if (!customerInstance) {
+			flash.message = "${message(code: 'customer.not.found.message', args: [message(code: 'customer.label', default: 'Customer'), params.id])}"
+			redirect(action: "list")
+		}
+		else {
+			[customerInstance: customerInstance]
+		}
+	}
+	
+	def edit = {
+		def customerInstance = Customer.get(params.id)
+		if (!customerInstance) {
+			flash.message = "${message(code: 'customer.not.found.message', args: [message(code: 'customer.label', default: 'Customer'), params.id])}"
+			redirect(action: "list")
+		}
+		else {
+			return [customerInstance: customerInstance]
+		}
+	}
+	
+	def update = {
+		def customerInstance = Customer.get(params.id)
+		if (customerInstance) {
+			customerInstance.properties = params
+			if (!customerInstance.hasErrors() && customerInstance.save(flush: true)) {
+				flash.message = "${message(code: 'customer.updated.message', args: [message(code: 'customer.label'), customerInstance.firstName, customerInstance.lastName])}"
+				redirect(action: "show", id: customerInstance.id)
+			}
+			else {
+				render(view: "edit", model: [customerInstance: customerInstance])
+			}
+		}
+		else {
+			flash.message = "${message(code: 'customer.not.found.message', args: [message(code: 'customer.label', default: 'Customer'), params.id])}"
+			redirect(action: "list")
+		}
+	}
+	
+	def delete = {
+		def customerInstance = Customer.get(params.id)
+		if (customerInstance) {
+			try {
+				customerInstance.delete(flush: true)
+				flash.message = "${message(code: 'customer.deleted.message', args: [message(code: 'customer.label'), customerInstance.firstName, customerInstance.lastName])}"
+				redirect(action: "list")
+			}
+			catch (org.springframework.dao.DataIntegrityViolationException e) {
+				flash.message = "${message(code: 'customer.not.deleted.message', args: [message(code: 'customer.label', default: 'Customer'), params.id])}"
+				redirect(action: "show", id: params.id)
+			}
+		}
+		else {
+			flash.message = "${message(code: 'customer.not.found.message', args: [message(code: 'customer.label', default: 'Customer'), params.id])}"
+			redirect(action: "list")
+		}
+	}
 	
 	def autoComplete = {
 		def customers = Customer.list()
@@ -98,7 +114,7 @@ class CustomerController {
 			[id: it.id, name: it.firstName]
 		}
 		def jsonCustomer = [
-		    result: customers
+		result: customers
 		]
 		render jsonCustomer as JSON
 	}
---
+++ pitstop/test/integration/no/pstop/webapp/CustomerControllerIntegrationTests.groovy
@@ -0,0 +1,44 @@
+package no.pstop.webapp
+import groovy.util.GroovyTestCase;
+
+import grails.test.*
+
+class CustomerControllerIntegrationTests extends GroovyTestCase {
+	Customer customer1, customer2, customer3
+	def controller
+	
+    protected void setUp() {
+        super.setUp()
+        customer1 = new Customer(firstName: "Dekk Roar", lastName: "Dekkesen", phoneNumber: "19555095", address: "Dekkveien 1",
+		postalCode: "0195", city: "Radiator By", email: "felger@dekk.no", company: "Dekkilicious", notice: "God kunde").save(flush: true)
+		
+		customer2 = new Customer(firstName: "Dekk Ingunn", lastName: "Felgesen", phoneNumber: "94950854", address: "Felgesvingen 8",
+		postalCode: "1347", city: "Kardang tettsted", email: "felger@felg.no", company: "Dekkilicious", notice: "Ganske god kunde").save(flush: true)
+		
+		customer3 = new Customer(firstName: "Hans Fredrik Olsen", lastName: "Felgesen", phoneNumber: "112", address: "Helgegata 8",
+		postalCode: "1347", city: "Kardang tettsted", email: "felger@felg.no", company: "Dekkpartner", notice: "Ganske god kunde").save(flush: true)
+		
+		controller = new CustomerController()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testListWithSearchQuery() {
+    	controller.params.q = "Dekkilicious"
+		def expectedList = [customer1, customer2]
+		def model = controller.list()
+		
+		assertEquals "Expected list should be the same as the returned", expectedList.toString(), model.customerInstanceList.toString()
+		assertEquals "Number of expected should be the same as returned", expectedList.size(), model.customerInstanceTotal
+    }
+	
+	void testListWithoutSearchQuery() {
+		def expectedList = [customer1, customer2, customer3]
+		def model = controller.list()
+		
+		assertEquals "Expected list should be the same as the returned", expectedList.toString(), model.customerInstanceList.toString()
+		assertEquals "Number of expected should be the same as returned", expectedList.size(), model.customerInstanceTotal
+	}
+}
