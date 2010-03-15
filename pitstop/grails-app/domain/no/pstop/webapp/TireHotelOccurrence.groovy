92d86ecbf98e03d720abc54a6d16133e45fd68a6
eirik.luka <eirik.luka@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-03-15 11:55:29 +0000

    Fikset regex i TireHotelOcc..

--- pitstop/grails-app/domain/no/pstop/webapp/TireHotelOccurrence.groovy
+++ pitstop/grails-app/domain/no/pstop/webapp/TireHotelOccurrence.groovy
@@ -15,7 +15,7 @@ class TireHotelOccurrence {
     static constraints = {
 		tireLocation(blank: false, nullable: false, matches:"([a-zA-ZøæåØÆÅ0-9])[a-zA-ZøæåØÆÅ0-9\\-\\_\\s]{0,20}")
 		registrationNumber(blank: false, nullable: false, matches:"[a-zA-ZøæåØÆÅ0-9]{0,10}[\\-\\s]?[a-zA-ZøæåØÆÅ0-9]{0,10}")
-		carType(matches:"[a-zA-ZøæåØÆÅ0-9\\-\\_\\s]{0,30}")
+		carType(matches:"[a-zA-ZøæåØÆÅ0-9\\-\\_\\s\\.]{0,30}")
 		tireType(inList:["Sommer","Vinter (pigg)","Vinter (piggfri)", "Vinter (pigg - upigget)", "M+S (helårsdekk)"], blank:false)
 		inDate(blank:false, nullable: false)
 		outDate(blank:true, nullable: true)
