7499a981701937f78a3ba0be1170feafe0b4c79e
oyvind.midtbo <oyvind.midtbo@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-03-16 13:51:48 +0000

    CustomerOrder, CustomerOrderLine og tester for shizzle

--- pitstop/grails-app/domain/no/pstop/webapp/CustomerOrder.groovy
+++ pitstop/grails-app/domain/no/pstop/webapp/CustomerOrder.groovy
@@ -3,6 +3,12 @@ package no.pstop.webapp
 class CustomerOrder {
 	static hasMany = [customerOrderLines: CustomerOrderLine]
     static belongsTo = [customer: Customer]
+    Customer customer
+    Date orderDate
+	String notice
+	
     static constraints = {
+		orderDate(max: new Date() + 1)
+		notice(size:0..1000)
     }
 }
