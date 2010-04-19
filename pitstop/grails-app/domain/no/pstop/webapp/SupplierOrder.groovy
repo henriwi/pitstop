d726d1c1f37ae61220aa9a4c992e431e4e7b3c12
oyvind.midtbo <oyvind.midtbo@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-04-19 11:37:31 +0000

    Lagt til tester på domenemodell

--- pitstop/grails-app/domain/no/pstop/webapp/SupplierOrder.groovy
+++ pitstop/grails-app/domain/no/pstop/webapp/SupplierOrder.groovy
@@ -1,7 +1,19 @@
 package no.pstop.webapp
 
 class SupplierOrder {
-
-    static constraints = {
+	static hasMany = [SupplierOrderLines: SupplierOrderLine]
+	Date orderDate
+	String supplier
+	String notice
+	
+	static constraints = {
+		// Hvis denne er med, vil det ikke være mulighet til å endre 
+		// dato senere dersom den skal være før dagens dato
+		//orderDate(max: new Date() + 1)
+		notice(size: 0..1000)
     }
+	
+	String toString(){
+		"Ordre - ${id}"
+	}
 }
--- pitstop/test/unit/no/pstop/webapp/SupplierOrderTests.groovy
+++ pitstop/test/unit/no/pstop/webapp/SupplierOrderTests.groovy
@@ -3,15 +3,30 @@ package no.pstop.webapp
 import grails.test.*
 
 class SupplierOrderTests extends GrailsUnitTestCase {
+	def supplierOrder
     protected void setUp() {
-        super.setUp()
+		super.setUp()
+		mockForConstraintsTests SupplierOrder
+		supplierOrder = new SupplierOrder(supplier: "Pirelli", orderDate: new Date(), notice: "")
     }
 
     protected void tearDown() {
         super.tearDown()
     }
 
-    void testSomething() {
-
-    }
+	void testGenerateCustomerOrderWithInvalidNotice() {
+		for(int i = 0; i < 2000; i++)
+		{
+			supplierOrder.notice += "A"
+		}
+		
+		assertFalse "Test failed, expected invalid notice", supplierOrder.validate()
+		def badField = supplierOrder.errors.getFieldError("notice")
+		assertNotNull "I'm expecting to find error in notice", badField
+	}
+	
+	void testToString() {
+		supplierOrder.id = 1
+		assertEquals "Ordre - 1", supplierOrder.toString()
+	}
 }
