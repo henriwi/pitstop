a732c52b36d1ffa2d8896b561ff65b354e510050
oyvind.midtbo <oyvind.midtbo@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-04-20 11:16:50 +0000

    Javascript-fil

---
+++ pitstop/web-app/js/script.js
@@ -0,0 +1,8 @@
+function showAndHideCustomerInfo() {
+	if (document.getElementById('customerShowDialog').style.visibility == "visible") { 
+		document.getElementById('customerShowDialog').style.visibility = "hidden";
+	}
+	else {
+		document.getElementById('customerShowDialog').style.visibility = "visible";
+	}
+}
