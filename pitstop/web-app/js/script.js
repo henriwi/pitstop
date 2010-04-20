cca49dcba881fe8cad37c7de2c776d60cfa7e599
oyvind.midtbo <oyvind.midtbo@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-04-20 11:33:12 +0000

    Nå vises element1, og element2 skjules, eller omvendt.

--- pitstop/web-app/js/script.js
+++ pitstop/web-app/js/script.js
@@ -1,8 +1,10 @@
-function showAndHideCustomerInfo() {
-	if (document.getElementById('customerShowDialog').style.visibility == "visible") { 
-		document.getElementById('customerShowDialog').style.visibility = "hidden";
+function hideAndShowElement(element1, element2) {
+	if (document.getElementById(element1).style.visibility == "visible") { 
+		document.getElementById(element1).style.visibility = "hidden";
+		document.getElementById(element2).style.visibility = "visible";
 	}
 	else {
-		document.getElementById('customerShowDialog').style.visibility = "visible";
+		document.getElementById(element1).style.visibility = "visible";
+		document.getElementById(element2).style.visibility = "hidden";
 	}
 }
