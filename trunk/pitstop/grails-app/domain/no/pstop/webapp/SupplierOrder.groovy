36ed154b73fe3217096e4cb41a534be9a7994219
jt.dalen <jt.dalen@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-04-24 11:43:49 +0000

    Lagt til constraints i supplierOrder og oppdatert tilhørende tester!

--- trunk/pitstop/grails-app/domain/no/pstop/webapp/SupplierOrder.groovy
+++ trunk/pitstop/grails-app/domain/no/pstop/webapp/SupplierOrder.groovy
@@ -5,11 +5,15 @@ class SupplierOrder {
 	String orderNumber
 	Date orderDate
 	String supplier
+	String notice
 	
 	static constraints = {
 		// Hvis denne er med, vil det ikke være mulighet til å endre 
 		// dato senere dersom den skal være før dagens dato
 		//orderDate(max: new Date() + 1)
+		notice(size: 0..1000)
+		orderNumber(size: 0..500, blank: false )
+		supplier(size: 0..100)
     }
 	
 }
