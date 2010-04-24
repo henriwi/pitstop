2692ff0153aac7336bbff682f15dd5af8f88b6f3
jt.dalen <jt.dalen@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-04-24 12:49:07 +0000

    Constraints på supplierOrderLine og lagt til tester på supplierOrderLine

--- pitstop/test/unit/no/pstop/webapp/SupplierOrderLineTests.groovy
+++ pitstop/test/unit/no/pstop/webapp/SupplierOrderLineTests.groovy
@@ -3,29 +3,37 @@ package no.pstop.webapp
 import grails.test.*
 
 class SupplierOrderLineTests extends GrailsUnitTestCase {
-	def tireOccurrence
+	//def tireOccurrence
 	def supplierOrderLine
 	
     protected void setUp() {
         super.setUp()
-		mockForConstraintsTests TireOccurrence
+		//mockForConstraintsTests TireOccurrence
 		mockForConstraintsTests SupplierOrderLine
-		tireOccurrence = new TireOccurrence(tire: new Tire(), price: 89.89, numberInStock: 4, numberOfReserved: 3,
-				numberOfOrdered: 8, discount: 20, environmentalFee: 10, registrationDate: new Date())
-		supplierOrderLine = new SupplierOrderLine(tireOccurrence: tireOccurrence, numberOfOrderedTireOccurrences: 4,
-				price: 950.0, deliveryDate: new Date())
+		/*tireOccurrence = new TireOccurrence(tire: new Tire(), price: 89.89, numberInStock: 4, numberOfReserved: 3,
+				numberOfOrdered: 8, discount: 20, environmentalFee: 10, registrationDate: new Date())*/
+		supplierOrderLine = new SupplierOrderLine(tire: new Tire(), supplierOrder: new SupplierOrder(), 
+		numberOfOrderedTires: 4,price: 950.0, receivedDate: new Date(), discount: 10, environmentalFee: 12)
     }
 
     protected void tearDown() {
         super.tearDown()
     }
 
-	void testGenerateSupplierOrderLineWithInvalidNumberOfOrderedTireOccurrences() {
-		supplierOrderLine.numberOfOrderedTireOccurrences = 0
+	void testGenerateSupplierOrderLineWithInvalidNumberOfOrderedTires() {
+		supplierOrderLine.numberOfOrderedTires = 0
 		
-		assertFalse "Test failed, expecting invalid numberOfOrderedTireOccurrences", supplierOrderLine.validate()
-		def badField = supplierOrderLine.errors.getFieldError('numberOfOrderedTireOccurrences')
-		assertNotNull "Expecting error in field numberOfOrderedTireOccurrences", badField
+		assertFalse "Test failed, expecting invalid numberOfOrderedTires", supplierOrderLine.validate()
+		def badField = supplierOrderLine.errors.getFieldError('numberOfOrderedTires')
+		assertNotNull "Expecting error in field numberOfOrderedTires", badField
+	}
+	
+	void testGenerateSupplierOrderLineWithEmptyNumberOfOrderedTires(){
+		supplierOrderLine.numberOfOrderedTires = null
+		
+		assertFalse "Test failed, expected empty numberOfOrderedTires", supplierOrderLine.validate()
+		def badField = supplierOrderLine.errors.getFieldError("numberOfOrderedTires")
+		assertNotNull "I'm expecting to find error in numberOfOrderedTires", badField
 	}
 	
 	void testGenerateSupplierOrderLineWithInvalidPrice() {
