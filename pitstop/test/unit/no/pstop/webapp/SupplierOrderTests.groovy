b1c956f4e93c30d21b21f2955acdda365df94d12
jt.dalen <jt.dalen@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-04-24 11:43:49 +0000

    Lagt til constraints i supplierOrder og oppdatert tilhørende tester!

--- pitstop/test/unit/no/pstop/webapp/SupplierOrderTests.groovy
+++ pitstop/test/unit/no/pstop/webapp/SupplierOrderTests.groovy
@@ -7,16 +7,15 @@ class SupplierOrderTests extends GrailsUnitTestCase {
     protected void setUp() {
 		super.setUp()
 		mockForConstraintsTests SupplierOrder
-		supplierOrder = new SupplierOrder(supplier: "Pirelli", orderDate: new Date(), notice: "")
+		supplierOrder = new SupplierOrder(orderNumber: "86789", orderDate: new Date(), supplier: "Pirelli", notice: "")
     }
 
     protected void tearDown() {
         super.tearDown()
     }
 
-	void testGenerateCustomerOrderWithInvalidNotice() {
-		for(int i = 0; i < 2000; i++)
-		{
+	void testGenerateSupplierOrderWithInvalidNotice() {
+		for(int i = 0; i < 2000; i++){
 			supplierOrder.notice += "A"
 		}
 		
@@ -25,8 +24,36 @@ class SupplierOrderTests extends GrailsUnitTestCase {
 		assertNotNull "I'm expecting to find error in notice", badField
 	}
 	
-	void testToString() {
+	void testGenerateSupplierOrderWithInvalidOrderNumber() {
+		for(int i = 0; i < 501; i++){
+			supplierOrder.orderNumber += "A"
+		}
+		
+		assertFalse "Test failed, expected invalid orderNumber", supplierOrder.validate()
+		def badField = supplierOrder.errors.getFieldError("orderNumber")
+		assertNotNull "I'm expecting to find error in orderNumber", badField
+	}
+	
+	void testGenerateSupplierOrderWithEmtyOrderNumber(){
+		supplierOrder.orderNumber = ""
+		
+		assertFalse "Test failed, expected empty orderNumber", supplierOrder.validate()
+		def badField = supplierOrder.errors.getFieldError("orderNumber")
+		assertNotNull "I'm expecting to find error in orderNumber", badField
+	}
+	
+	void testGenerateSupplierOrderWithInvalidSupplier() {
+		for(int i = 0; i < 100; i++){
+			supplierOrder.supplier += "A"
+		}
+		
+		assertFalse "Test failed, expected invalid supplier", supplierOrder.validate()
+		def badField = supplierOrder.errors.getFieldError("supplier")
+		assertNotNull "I'm expecting to find error in supplier", badField
+	}
+	
+	/*void testToString() {
 		supplierOrder.id = 1
 		assertEquals "Ordre - 1", supplierOrder.toString()
-	}
+	}*/
 }
