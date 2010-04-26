ed8e4fccc4599a455480b7724c196843a75c5c0f
jt.dalen <jt.dalen@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-04-26 07:27:34 +0000

    Lagt til indexController

---
+++ pitstop/test/unit/no/pstop/webapp/IndexControllerTests.groovy
@@ -0,0 +1,31 @@
+package no.pstop.webapp
+
+import grails.test.*
+
+class IndexControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+	
+	void testRanges(){
+		println barSize([s:'b',a:1,d:3])
+	}
+	def barSize = {args ->
+		def output = 'chbh='
+		output << listToString(args.values())
+	}
+	def listToString(list){
+		replaceSpaces('',list.toString()[1..-2])
+	}
+	def replaceSpaces(replacement, string){
+		string.replaceAll(/\s/){replacement}
+	}
+}
