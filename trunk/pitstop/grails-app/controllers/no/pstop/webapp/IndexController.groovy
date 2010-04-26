f11a51dbb6d717135227df6a362c02169231d266
jt.dalen <jt.dalen@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-04-26 07:27:34 +0000

    Lagt til indexController

---
+++ trunk/pitstop/grails-app/controllers/no/pstop/webapp/IndexController.groovy
@@ -0,0 +1,20 @@
+package no.pstop.webapp
+
+class IndexController {
+    def index = {
+		def tireList = Tire.list()
+		def tireLabelList = []
+		def numberOfAvailableList = []
+		tireList.each {
+			int numberOfAvailable = 0
+			tireLabelList << it.brand +" " +it.tireName +" " +it.diameter +"''"
+			it.tireOccurrences.each {
+				numberOfAvailable += it.numberOfAvailable()
+			}
+			numberOfAvailableList << numberOfAvailable
+		}
+		println numberOfAvailableList
+		println tireLabelList
+		render(model: [numberOfAvailableListValues: numberOfAvailableList, tireListLabels: tireLabelList], view: "index")
+	}
+}
