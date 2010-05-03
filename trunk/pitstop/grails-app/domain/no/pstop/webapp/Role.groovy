175e26a054defd3b5f47a5d5618c97b15a9d2670
henrikwingerei <henrikwingerei@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-05-03 10:46:54 +0000

    Fjernet noen kommentarer

--- trunk/pitstop/grails-app/domain/no/pstop/webapp/Role.groovy
+++ trunk/pitstop/grails-app/domain/no/pstop/webapp/Role.groovy
@@ -2,16 +2,10 @@ package no.pstop.webapp
 
 import no.pstop.webapp.User
 
-/**
- * Authority domain class.
- */
 class Role {
-
 	static hasMany = [people: User]
-
-	/** description */
+	                  
 	String description
-	/** ROLE String */
 	String authority
 
 	static constraints = {
