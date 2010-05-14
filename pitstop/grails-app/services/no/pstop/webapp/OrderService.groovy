596b3491880fe8261ce6e4c1dcd8d6ebd279d6cf
henrikwingerei <henrikwingerei@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-05-14 19:44:36 +0000

    Endret litt i orderService.

--- pitstop/grails-app/services/no/pstop/webapp/OrderService.groovy
+++ pitstop/grails-app/services/no/pstop/webapp/OrderService.groovy
@@ -4,32 +4,20 @@ class OrderService {
 	static transactional = true
 
     def saveSupplierOrder(orderInstance, session) throws RuntimeException{
-		if(!orderInstance.save(flush:true)){
-			orderInstance.errors.each { 
-				println it
-			}
-			throw new RuntimeException("cannot save order")
-		}
 		session.orderLines.each {
-			it.supplierOrder = orderInstance
-			if(!it.save(flush: true)){
-				it.errors.each { 
-					println it
-				}
-				throw new RuntimeException("cannot save orderline")
-			}
+			orderInstance.addToSupplierOrderLines(it)
+		}
+		if(!orderInstance.save()) {
+			throw new RuntimeException("cannot save supplierOrder")
 		}
     }
 	
     def saveCustomerOrder(orderInstance, session) throws RuntimeException{
-		if(!orderInstance.save(flush:true)){
-			throw new RuntimeException("cannot save order")
-		}
 		session.orderLines.each {
-			it.customerOrder  = orderInstance
-			if(!it.save(flush: true)){
-				throw new RuntimeException("cannot save orderline")
-			}
+			orderInstance.addToCustomerOrderLines(it)
+		}
+		if(!orderInstance.save()) {
+			throw new RuntimeException("cannot save customerOrder")
 		}
     }
 }
