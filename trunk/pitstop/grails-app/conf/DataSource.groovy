8c00c45e9553faf8371ad4491bd01f51ff741a84
henrikwingerei <henrikwingerei@bfef11ca-ff94-11de-8cb8-1d395499b42c>
2010-05-14 13:24:16 +0000

    Update

--- trunk/pitstop/grails-app/conf/DataSource.groovy
+++ trunk/pitstop/grails-app/conf/DataSource.groovy
@@ -29,7 +29,7 @@ environments {
 	}
 	production {
 		dataSource {
-			dbCreate = "create-drop"
+			dbCreate = "update"
 			url = "jdbc:mysql://localhost/pitstop_prod?removeAbandoned=true&removeAbandonedTimeout=120" +
 			"&logAbandoned=true&testOnBorrow=false&testOnReturn=false&timeBetweenEvictionRunsMillis=60000" + 
 			"&numTestsPerEvictionRun=5&minEvictableIdleTimeMillis=30000&testWhileIdle=true&validationQuery=select now()"
